89e515d2ef4d35d63a71a52ba9adf8330e7f7c7c
Arpit Singh <arpit@totalitycorp.com>
2020-04-05 22:02:41 +0530

    Adding option for activity and workflow worker

--- cmd/main.go
+++ cmd/main.go
@@ -15,13 +15,11 @@ func main() {
 	var serverPort string
 	var tasklist string
 	var logger string
-	var workerType string
 	flag.StringVar(&service, "service", "", "Name of the service to start (app server or workflow workerworker)")
 	flag.StringVar(&configFilePath, "config", "./config", "absolute path to the configuration file")
 	flag.StringVar(&serverPort, "port", "4000", "port on which server runs")
 	flag.StringVar(&tasklist, "tasklist", "", "Name of the tasklist")
 	flag.StringVar(&logger, "v", "0", "Logger enable/disable")
-	flag.StringVar(&workerType, "type", "", "Type of worker {activity/workflow}")
 	flag.Parse()
 
 	if os.Getenv("TASKLIST") != "" {
@@ -32,27 +30,21 @@ func main() {
 		logger = os.Getenv("VERBOSE")
 	}
 
-	if os.Getenv("WORKER_TYPE") != "" {
-		workerType = os.Getenv("WORKER_TYPE")
-	}
-
 	if service == "app" || os.Getenv("SERVICE") == "app" {
 		application := server.New(configFilePath)
+
 		// init necessary module before start
 		application.Init()
 
-		// bootstrap workflow worker
-		worker := worker.New(configFilePath)
-		worker.Init(tasklist)
-		worker.Start(logger, workerType)
-
 		// start http server
 		application.Start(serverPort)
-	} else if service == "worker" || os.Getenv("SERVICE") == "worker" {
-
+	} else if service == "workflow" || os.Getenv("SERVICE") == "workflow" {
 		worker := worker.New(configFilePath)
 		worker.Init(tasklist)
-		worker.Start(logger, workerType)
-		// The workers are supposed to be long running process that should not exit.
+		worker.Start(logger, service)
+	} else if service == "activity" || os.Getenv("SERVICE") == "activity" {
+		worker := worker.New(configFilePath)
+		worker.Init(tasklist)
+		worker.Start(logger, service)
 	}
 }
--- cmd/worker/worker.go
+++ cmd/worker/worker.go
@@ -1,9 +1,6 @@
 package worker
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/YOVO-LABS/workflow/config"
 	ca "github.com/YOVO-LABS/workflow/internal/adapter"
 
@@ -46,28 +43,24 @@ func (w *Worker) Init(tasklist string) {
 func (w *Worker) Start(verbose, workerType string) {
 	// Configure worker options.
 	workerOptions := worker.Options{
-		MetricsScope:                      w.cadenceAdapter.Scope,
-		EnableLoggingInReplay:             true,
-		MaxConcurrentSessionExecutionSize: 1,
+		MetricsScope:          w.cadenceAdapter.Scope,
+		EnableLoggingInReplay: true,
 	}
-	if workerType == "workflow" {
-		fmt.Println("Workflow worker")
-		workerOptions.DisableWorkflowWorker = false
-		workerOptions.DisableActivityWorker = true
-		workerOptions.EnableSessionWorker = false
-		workerOptions.WorkerStopTimeout = time.Second * 10
-	} else if workerType == "activity" {
-		fmt.Println("Activity worker")
-		workerOptions.DisableWorkflowWorker = true
-		workerOptions.DisableActivityWorker = false
-		workerOptions.EnableSessionWorker = true
-		workerOptions.WorkerStopTimeout = time.Second * 10
-	}
-
 	if verbose == "0" {
 		workerOptions.Logger = zap.NewNop()
 	} else {
 		workerOptions.Logger = w.cadenceAdapter.Logger
+	}
+
+	if workerType == "activity" {
+		workerOptions.EnableSessionWorker = true
+		workerOptions.DisableWorkflowWorker = true
+		workerOptions.DisableActivityWorker = false
+		workerOptions.MaxConcurrentSessionExecutionSize = 1
+	} else if workerType == "workflow" {
+		workerOptions.EnableSessionWorker = false
+		workerOptions.DisableWorkflowWorker = false
+		workerOptions.DisableActivityWorker = true
 	}
 
 	cadenceWorker := worker.New(w.cadenceAdapter.ServiceClient, w.config.Cadence.Domain, w.taskList, workerOptions)
